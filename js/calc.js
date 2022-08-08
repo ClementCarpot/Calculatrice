5a2d2d437cc89ae8ebbc1de852ac4bcd6552fae9
Clement Carpot <clementcarpot@gmail.com>
2022-08-08 22:11:08 +0200

    maj 08/08/2022

--- js/calc.js
+++ js/calc.js
@@ -2,13 +2,13 @@
 function calculer()
 {
     let a = document.getElementById("affichage").value 
-    let b = eval(a) 
+    let b = eval(a.replace('×', '*').replace('−', '-')) 
     document.getElementById("affichage").value = b 
 }
 
 function afficher(val)
 {
-    document.getElementById("affichage").value+=val;
+    document.getElementById("affichage").value+=val.replace('*', '×').replace('-', '−');
 }
 
 function effacer()
